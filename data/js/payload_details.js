c9fd6ef954fa3457a0016b0fb3bbdeff51083431
pjskyr <56811090+pjskyr@users.noreply.github.com>
2019-10-21 12:50:55 +0900

    Initial

---
+++ data/js/payload_details.js
@@ -0,0 +1,5 @@
+var payload_details =  {
+  "tweets" : 47268,
+  "created_at" : "2019-04-14 15:39:19 +0000",
+  "lang" : "ko"
+}
---
+++ data/js/tweets/2018_07.js
@@ -0,0 +1,156 @@
+Grailbird.data.tweets_2018_07 = 
+ [ {
+  "source" : "\u003Ca href=\"http:\/\/www.echofon.com\" rel=\"nofollow\"\u003EEchofon  Android\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "1023290231447908355",
+  "text" : "\uC77C\uB2E8\uC740 \uD280\uAE40\uC18C\uB9AC\uB780 \uB9D0\uC5D0 \uBE75\uD130\uC9C4 \uBAA8\uC2B5\uC73C\uB85C \uD790\uB9C1\uC744 \uD558\uACE0 \uB0B4\uC77C \uB9C8\uC800 \uB2E4 \uBCF4\uB3C4\uB85D \uD574\uC57C\uACA0\uB2E4... \uC624\uB298 \uC0AC\uC0DD \uC0AC\uAC74 \uB4E3\uACE0 \uB108\uBB34 \uD654\uAC00 \uB9CE\uC774 \uB0AC\uB294\uB370 \uD790\uB9C1\uD574\uC57C\uC9C0 \uD790\uB9C1",
+  "id" : 1023290231447908355,
+  "created_at" : "2018-07-28 19:32:57 +0000",
+  "user" : {
+    "name" : "PIA\u00B9\u00B9",
+    "screen_name" : "WnO_nB",
+    "protected" : true,
+    "id_str" : "1021475790607663104",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/1085840313334169600\/yXU9nYsM_normal.jpg",
+    "id" : 1021475790607663104,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"http:\/\/www.echofon.com\" rel=\"nofollow\"\u003EEchofon  Android\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "1023289581842587648",
+  "text" : "\uCEF4\uD4E8\uD130 \uC5C5\uB370\uC774\uD2B8\uD558\uB2E4\uAC00 \uC878\uB824\uC11C \uC7A0\uB4E4\uC5C8\uB294\uB370 \uD558\uD544 \uADF8 \uC0AC\uC774\uC5D0 \uBE28\uC571\uC774\uB77C\uB2C8....",
+  "id" : 1023289581842587648,
+  "created_at" : "2018-07-28 19:30:22 +0000",
+  "user" : {
+    "name" : "PIA\u00B9\u00B9",
+    "screen_name" : "WnO_nB",
+    "protected" : true,
+    "id_str" : "1021475790607663104",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/1085840313334169600\/yXU9nYsM_normal.jpg",
+    "id" : 1021475790607663104,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"http:\/\/www.echofon.com\" rel=\"nofollow\"\u003EEchofon  Android\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "1022485729262198789",
+  "text" : "\uC55C\u314B\u314B\u314B\u314B 105\uC988 (\uB77C \uC4F0\uACE0 110\uC988\uB77C \uC77D\uB294\uB2E4) \uBAA9\uACA9\uC0AC\uC9C4 \uACAC\uC8FC\uC640 \uC0B0\uCC45 \uB098\uAC00\uC11C \uC2E0\uB09C \uBA48\uBB34 \uADF8 \uC790\uCCB4\uC796\uC544 \u314B\u314B\u314B\u314B\u314B\u314B\u314B\u314B",
+  "id" : 1022485729262198789,
+  "created_at" : "2018-07-26 14:16:09 +0000",
+  "user" : {
+    "name" : "PIA\u00B9\u00B9",
+    "screen_name" : "WnO_nB",
+    "protected" : true,
+    "id_str" : "1021475790607663104",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/1085840313334169600\/yXU9nYsM_normal.jpg",
+    "id" : 1021475790607663104,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"http:\/\/www.echofon.com\" rel=\"nofollow\"\u003EEchofon  Android\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "1022123863159627776",
+  "text" : "\uC7AC\uD658\uC774 \uBE28\uC571 \uC9C4\uC9DC \uD790\uB9C1\uC774\uC5C8\uB2E4... \uACE0\uB9C8\uC6CC \u3160\u3160\u3160\u3160",
+  "id" : 1022123863159627776,
+  "created_at" : "2018-07-25 14:18:13 +0000",
+  "user" : {
+    "name" : "PIA\u00B9\u00B9",
+    "screen_name" : "WnO_nB",
+    "protected" : true,
+    "id_str" : "1021475790607663104",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/1085840313334169600\/yXU9nYsM_normal.jpg",
+    "id" : 1021475790607663104,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/mobile.twitter.com\" rel=\"nofollow\"\u003ETwitter Web App\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "1021739589717749760",
+  "text" : "\uB0A0\uC528 \uC9C4\uC9DC \uB365\uB2E4",
+  "id" : 1021739589717749760,
+  "created_at" : "2018-07-24 12:51:15 +0000",
+  "user" : {
+    "name" : "PIA\u00B9\u00B9",
+    "screen_name" : "WnO_nB",
+    "protected" : true,
+    "id_str" : "1021475790607663104",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/1085840313334169600\/yXU9nYsM_normal.jpg",
+    "id" : 1021475790607663104,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/mobile.twitter.com\" rel=\"nofollow\"\u003ETwitter Web App\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "1021630525532909568",
+  "text" : "\uB610 \uAC00\uB2C8... \uC798 \uB2E4\uB140\uC640...",
+  "id" : 1021630525532909568,
+  "created_at" : "2018-07-24 05:37:52 +0000",
+  "user" : {
+    "name" : "PIA\u00B9\u00B9",
+    "screen_name" : "WnO_nB",
+    "protected" : true,
+    "id_str" : "1021475790607663104",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/1085840313334169600\/yXU9nYsM_normal.jpg",
+    "id" : 1021475790607663104,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"http:\/\/twitter.com\" rel=\"nofollow\"\u003ETwitter Web Client\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "1021484899897954304",
+  "text" : "\uB0B4 \uD3B8\uC758\uB97C \uC704\uD55C \uB9AC\uD2B8\uC717 \uACC4\uC815",
+  "id" : 1021484899897954304,
+  "created_at" : "2018-07-23 19:59:13 +0000",
+  "user" : {
+    "name" : "PIA\u00B9\u00B9",
+    "screen_name" : "WnO_nB",
+    "protected" : true,
+    "id_str" : "1021475790607663104",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/1085840313334169600\/yXU9nYsM_normal.jpg",
+    "id" : 1021475790607663104,
+    "verified" : false
+  }
+} ]

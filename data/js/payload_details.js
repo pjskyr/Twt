934789db7b2e498b4794268fd729a1d97f4f5a46
pjskyr <56811090+pjskyr@users.noreply.github.com>
2020-02-04 01:54:14 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50379,
-	"created_at": "Mon Jan 27 00:54:11 +0000 2020",
+	"tweets": 50380,
+	"created_at": "Mon Feb 03 16:54:11 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,212 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Mon Feb 03 16:10:33 +0000 2020",
+		"id": 1216249135256371200,
+		"id_str": "1224364560146026498",
+		"full_text": "191222 #강다니엘 \n마지막에 앙 다문 입술까지 꼭 봐야 함 귀여우니까\nhttps://t.co/maf09X51tK https://t.co/twDyV1ruz2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			65
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "강다니엘",
+					"indices": [
+						7,
+						12
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/maf09X51tK",
+					"expanded_url": "https://bit.ly/3a0UHJZ",
+					"display_url": "bit.ly/3a0UHJZ",
+					"indices": [
+						42,
+						65
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1216249097155276800,
+					"id_str": "1216249097155276801",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EOD8HCJUcAEm4Tw.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EOD8HCJUcAEm4Tw.jpg",
+					"url": "https://t.co/twDyV1ruz2",
+					"display_url": "pic.twitter.com/twDyV1ruz2",
+					"expanded_url": "https://twitter.com/12and10gif/status/1216249135256371201/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 510,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1216249097155276800,
+					"id_str": "1216249097155276801",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EOD8HCJUcAEm4Tw.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EOD8HCJUcAEm4Tw.jpg",
+					"url": "https://t.co/twDyV1ruz2",
+					"display_url": "pic.twitter.com/twDyV1ruz2",
+					"expanded_url": "https://twitter.com/12and10gif/status/1216249135256371201/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 510,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							50,
+							51
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EOD8HCJUcAEm4Tw.mp4"
+							}
+						]
+					},
+					"features": {}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1215157114881990700,
+			"id_str": "1215157114881990657",
+			"name": "▲11▼",
+			"screen_name": "12and10gif",
+			"location": "",
+			"description": "fan account / 너무 좋아서 뭐라도 해보려고..",
+			"url": "https://t.co/zvTfHV5Lqp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zvTfHV5Lqp",
+							"expanded_url": "https://12and10.tistory.com/",
+							"display_url": "12and10.tistory.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 426,
+			"friends_count": 2,
+			"listed_count": 11,
+			"created_at": "Thu Jan 09 06:23:36 +0000 2020",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 49,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1215246710974238721/TXRol4gL_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1215246710974238721/TXRol4gL_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1215157114881990657/1578568732",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 16,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "191222 #강다니엘 \n마지막에 앙 다문 입술까지 꼭 봐야 함 귀여우니까\nhttps://t.co/maf09X51tK https://t.co/twDyV1ruz2"
+	}
+]

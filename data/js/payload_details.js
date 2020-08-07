5db061b0d4bb22e1cea3ae1764326a50f671d7cc
pjskyr <56811090+pjskyr@users.noreply.github.com>
2020-08-07 14:54:13 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50380,
-	"created_at": "Mon Feb 03 16:54:11 +0000 2020",
+	"tweets": 50381,
+	"created_at": "Fri Aug 07 05:54:12 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_08.js
@@ -0,0 +1,85 @@
+Grailbird.data.tweets_2020_08 = 
+[
+	{
+		"created_at": "Fri Aug 07 05:06:19 +0000 2020",
+		"id": 1291601542072549400,
+		"id_str": "1291601542072549377",
+		"full_text": "우리원 3주년 축하해!🎉🎉🎉 11명 건강하자 꽃길 걷자",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			30
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.echofon.com\" rel=\"nofollow\">Echofon  Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1021475790607663100,
+			"id_str": "1021475790607663104",
+			"name": "PIA¹¹",
+			"screen_name": "WnO_nB",
+			"location": "",
+			"description": "WannaOne ∞ WannaBle ♥ Fan Account\n★1¹¹=1 (POWER OF DESTINY)★ \n저장을 위한 RT+구독계 \n[워너원 11명 모두의 꽃길을 응원해]\n☞ 예전글부터 쭉 연속으로 많이 RT하는 경우 꽤 있어요. 놀라지들 않으시길😃\n☞ RT당첨이벤트 참여X",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 0,
+			"friends_count": 427,
+			"listed_count": 0,
+			"created_at": "Mon Jul 23 19:23:01 +0000 2018",
+			"favourites_count": 49124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 59463,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1085840313334169600/yXU9nYsM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1085840313334169600/yXU9nYsM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1021475790607663104/1554381392",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "우리원 3주년 축하해!🎉🎉🎉 11명 건강하자 꽃길 걷자"
+	}
+]

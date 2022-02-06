101d92b4765664a70194985063363f84a78ddcf3
pjskyr <56811090+pjskyr@users.noreply.github.com>
2022-02-06 10:54:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50420,
-	"created_at": "Thu Jan 27 13:54:11 +0000 2022",
+	"tweets": 50421,
+	"created_at": "Sun Feb 06 01:54:11 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_02.js
@@ -0,0 +1,217 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Sun Feb 06 01:32:33 +0000 2022",
+		"id": 1489886523222757400,
+		"id_str": "1490136296219885569",
+		"full_text": "Wanna One l 오케워너원 Special '2021 MAMA 대기실 비하인드'\n\n@ https://t.co/ugYdihHrPq\n\n#WannaOne #워너원 #오케워너원 #ㅇㅋㅇㄴㅇ https://t.co/rQP7DQM8K0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			103
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WannaOne",
+					"indices": [
+						75,
+						84
+					]
+				},
+				{
+					"text": "워너원",
+					"indices": [
+						85,
+						89
+					]
+				},
+				{
+					"text": "오케워너원",
+					"indices": [
+						90,
+						96
+					]
+				},
+				{
+					"text": "ㅇㅋㅇㄴㅇ",
+					"indices": [
+						97,
+						103
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ugYdihHrPq",
+					"expanded_url": "https://youtu.be/djYe45a3MCw",
+					"display_url": "youtu.be/djYe45a3MCw",
+					"indices": [
+						50,
+						73
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1489857967659057200,
+					"id_str": "1489857967659057152",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/media/FK0J7ZAagAALb-i.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FK0J7ZAagAALb-i.jpg",
+					"url": "https://t.co/rQP7DQM8K0",
+					"display_url": "pic.twitter.com/rQP7DQM8K0",
+					"expanded_url": "https://twitter.com/WannaOne_twt/status/1489886523222757383/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1057,
+							"h": 593,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1057,
+							"h": 593,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 381,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1489857967659057200,
+					"id_str": "1489857967659057152",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/media/FK0J7ZAagAALb-i.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FK0J7ZAagAALb-i.jpg",
+					"url": "https://t.co/rQP7DQM8K0",
+					"display_url": "pic.twitter.com/rQP7DQM8K0",
+					"expanded_url": "https://twitter.com/WannaOne_twt/status/1489886523222757383/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1057,
+							"h": 593,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1057,
+							"h": 593,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 381,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 872005518084251600,
+			"id_str": "872005518084251648",
+			"name": "Wanna One",
+			"screen_name": "WannaOne_twt",
+			"location": "",
+			"description": "Wanna One (워너원) 공식 트위터 계정 입니다.",
+			"url": "https://t.co/96qiM3GVXA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/96qiM3GVXA",
+							"expanded_url": "https://www.youtube.com/wannaoneofficial",
+							"display_url": "youtube.com/wannaoneoffici…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2866423,
+			"friends_count": 0,
+			"listed_count": 3124,
+			"created_at": "Tue Jun 06 08:21:31 +0000 2017",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1167,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1485463688412758020/YejTKtty_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1485463688412758020/YejTKtty_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/872005518084251648/1642997120",
+			"profile_link_color": "77C8C0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14746,
+		"favorite_count": 28183,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Wanna One l 오케워너원 Special '2021 MAMA 대기실 비하인드'\n\n@ https://t.co/ugYdihHrPq\n\n#WannaOne #워너원 #오케워너원 #ㅇㅋㅇㄴㅇ https://t.co/rQP7DQM8K0"
+	}
+]
